070142e23989653a196e59888d83d9444db735c4
Tinarst <teanarostamkhani@gmail.com>
2025-12-17 23:40:50 +0330

    add apps to installed_app, define media root and auth user

--- customi/source/settings.py
+++ customi/source/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'user',
+    'stock',
+    'cart',
+    'order',
+    'transaction'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +131,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.CustomUser'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+PRODUCT_MEDIA = MEDIA_ROOT / 'products'
+STORE_MEDIA = MEDIA_ROOT / 'store'
