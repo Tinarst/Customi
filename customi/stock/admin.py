f59e3f04a4549d82c758bb4e4742f5d9b2848e99
Tinarst <teanarostamkhani@gmail.com>
2025-12-30 18:55:18 +0330

    register stock models to admin site

--- customi/stock/admin.py
+++ customi/stock/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from stock.models import Product, ProductStore, Store, Category, Feedback
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(ProductStore)
+admin.site.register(Store)
+admin.site.register(Category)
+admin.site.register(Feedback)
