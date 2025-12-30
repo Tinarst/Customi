8d260caecb0057202fa7265fa05948a2ab560c46
Tinarst <teanarostamkhani@gmail.com>
2025-12-30 18:50:49 +0330

    move payment model to transaction

--- customi/transaction/models.py
+++ customi/transaction/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from core.models.base import BaseConfig
+from order.models import Order
 
-# Create your models here.
+class Payment(BaseConfig):
+
+    class PaymentStatus(models.TextChoices):
+        PAID = "paid", _("Paid")
+        CANCELED = "canceled", _("Canceled")
+
+    order = models.OneToOneField(Order, on_delete=models.PROTECT, unique=True)
+    status = models.CharField(max_length=8, default=PaymentStatus.PAID, choices=PaymentStatus)
