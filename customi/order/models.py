50a2ef7c5be0c545e75898faf3bdfec71d5b0967
Tinarst <teanarostamkhani@gmail.com>
2025-12-30 18:56:32 +0330

    complete order models

--- customi/order/models.py
+++ customi/order/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from user.models import CustomUser, Address
+from account.models import CustomUser, Address
 from cart.models import Coupon
 from stock.models import ProductStore
-from datetime import datetime
+from core.models.base import BaseConfig
 
 
-class Order(models.Model):
+class Order(BaseConfig):
 
     class OrderStatus(models.TextChoices):
         PENDING = "pending", _("Pending")
@@ -16,30 +16,17 @@ class Order(models.Model):
         CANCELED = "canceled", _("Canceled")
 
     user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=10, choices=OrderStatus)
     shipping_address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True)
-    coupon = models.ForeignKey(Coupon, on_delete=models.PROTECT, null=True)
+    coupon = models.ForeignKey(Coupon, on_delete=models.DO_NOTHING, null=True, blank=True)
     total_amount = models.FloatField()
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product_store = models.ForeignKey(ProductStore, on_delete=models.PROTECT)
-    quantity = models.PositiveSmallIntegerField()
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="orderitem_order")
+    product_store = models.ForeignKey(ProductStore, on_delete=models.SET_NULL, null=True, related_name="orderitem_productstore")
+    quantity = models.PositiveSmallIntegerField(editable=False)
     price = (
         models.FloatField()
     )  # do not mirroring; if one product update, it'll be change
-
-
-class Payment(models.Model):
-
-    class PaymentStatus(models.TextChoices):
-        PAID = "paid", _("Paid")
-        CANCELED = "canceled", _("Canceled")
-
-    order = models.OneToOneField(Order, on_delete=models.PROTECT, unique=True)
-    status = models.CharField(max_length=8, default=PaymentStatus.PAID, choices=PaymentStatus)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateField(auto_now_add=True, auto_now=True)
     
