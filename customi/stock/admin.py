997bb2b327bb5a02c9986e08a93590f23e22dade
Tinarst <teanarostamkhani@gmail.com>
2026-01-01 23:55:36 +0330

    register feedback models to admin site

--- customi/stock/admin.py
+++ customi/stock/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from stock.models import Product, ProductStore, Store, Category, Feedback
+from stock.models import Product, ProductImage, ProductStore, Store, Category, StoreFeedback, ProductFeedback
 
 admin.site.register(Product)
+admin.site.register(ProductImage)
 admin.site.register(ProductStore)
 admin.site.register(Store)
 admin.site.register(Category)
-admin.site.register(Feedback)
+admin.site.register(StoreFeedback)
+admin.site.register(ProductFeedback)
