ec0f155ad2757ba74f283226e1dad8a69db9adf9
Tinarst <teanarostamkhani@gmail.com>
2026-02-10 15:46:25 +0330

    add authority to transaction model to uniqe handling from zarinpal

--- customi/transaction/models.py
+++ customi/transaction/models.py
@@ -6,8 +6,15 @@ from order.models import Order
 class Payment(BaseConfig):
 
     class PaymentStatus(models.TextChoices):
+        PENDING = "pending", _("Pending")
         PAID = "paid", _("Paid")
         CANCELED = "canceled", _("Canceled")
 
-    order = models.OneToOneField(Order, on_delete=models.PROTECT, unique=True)
-    status = models.CharField(max_length=8, default=PaymentStatus.PAID, choices=PaymentStatus)
+    order = models.OneToOneField(
+        Order,
+        on_delete=models.PROTECT,
+        unique=True,
+        related_name="payment_order"
+    )
+    status = models.CharField(max_length=8, default=PaymentStatus.PENDING, choices=PaymentStatus)
+    authority = models.CharField(null=True)
