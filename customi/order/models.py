32425e71f31ad362ad9c877b8053dfbc1eca1411
Tinarst <teanarostamkhani@gmail.com>
2026-02-10 15:43:07 +0330

    change order status to numeric choices and customize order manager with <create> method

--- customi/order/models.py
+++ customi/order/models.py
@@ -6,27 +6,70 @@ from stock.models import ProductStore
 from core.models.base import BaseConfig
 
 
+class OrderManager(models.Manager):
+    def create(self, user: CustomUser, shipping_address):
+        cart = user.cart_user
+        total_amount = cart.total_price
+        order = Order(
+            user=user,
+            status=Order.OrderStatus.PENDING,
+            shipping_address=shipping_address,
+            total_amount=total_amount,
+        )
+
+        order.save()
+
+        for item in cart.cartitem_cart.all():
+            OrderItem.objects.create(
+                order=order,
+                product_store=item.product_store,
+                quantity=item.quantity,
+                price=item.total_price,
+            )
+        return order
+
+
 class Order(BaseConfig):
 
-    class OrderStatus(models.TextChoices):
-        PENDING = "pending", _("Pending")
-        PAID = "paid", _("Paid")
-        SHIPPED = "shipped", _("Shipped")
-        DELIVERIED = "deliveried", _("Deliveried")
-        CANCELED = "canceled", _("Canceled")
+    class OrderStatus(models.IntegerChoices):
+        PENDING = 1, _("Pending")
+        PAID = 2, _("Paid")
+        SHIPPED = 3, _("Shipped")
+        CANCELED = 4, _("Canceled")
 
-    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.SET_NULL, null=True, related_name="order_user"
+    )
     status = models.CharField(max_length=10, choices=OrderStatus)
-    shipping_address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True)
-    coupon = models.ForeignKey(Coupon, on_delete=models.DO_NOTHING, null=True, blank=True)
+    shipping_address = models.ForeignKey(
+        Address,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="order_shipping_address",
+    )
+    coupon = models.ForeignKey(
+        Coupon, on_delete=models.DO_NOTHING, null=True, blank=True
+    )
     total_amount = models.FloatField()
+
+    objects = OrderManager()
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="orderitem_order")
-    product_store = models.ForeignKey(ProductStore, on_delete=models.SET_NULL, null=True, related_name="orderitem_productstore")
-    quantity = models.PositiveSmallIntegerField(editable=False)
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="orderitem_order"
+    )
+    product_store = models.ForeignKey(
+        ProductStore,
+        on_delete=models.DO_NOTHING,
+        null=True,
+        related_name="orderitem_productstore",
+    )
+    quantity = models.PositiveSmallIntegerField(default=1, editable=False)
     price = (
         models.FloatField()
     )  # do not mirroring; if one product update, it'll be change
-    
+
+    @property
+    def total_price(self):
+        return self.quantity * self.price
