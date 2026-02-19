e2d12141a3206571e1012e33d4246baf368372a2
Tinarst <teanarostamkhani@gmail.com>
2026-02-19 20:23:32 +0330

    add logger and checker for ammount between 10k - 200m

--- customi/order/views.py
+++ customi/order/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
+from rest_framework.mixins import RetrieveModelMixin, ListModelMixin, UpdateModelMixin
 from django_filters.rest_framework import DjangoFilterBackend
 from django_filters.filterset import FilterSet
 import requests
@@ -13,7 +14,11 @@ from account.models import Address
 from cart.views import empty_cart
 from order.serializers import OrderSerializer, OrderItemSerializer
 from order.models import Order, OrderItem
+from core.permissions import OrderItemSeller
 from transaction.models import Payment
+import logging
+
+logger = logging.getLogger('customi')
 
 
 class ZarinpallPayment:
@@ -30,6 +35,12 @@ class ZarinpallPayment:
 
     def request(self) -> bool:
         """if can connect to client server True else False"""
+        if self.amount >= 200000000:
+            print("Sandbox does not handle over 200M")
+            return False
+        if self.amount < 1000:
+            print("Sandbox handle at least 1000")
+            return False
         data = {
             "merchant_id": str(self.merchant_id),
             "amount": int(self.amount) * 10,
@@ -43,7 +54,7 @@ class ZarinpallPayment:
             headers=ZarinpallPayment.__HEADER,
         )
         response_data = response.json()["data"]
-
+        print(response.json())
         if response_data.get("message", None) == "Success":
             self.authority = response_data["authority"]
             self.code = response_data["code"]
@@ -61,6 +72,7 @@ class ZarinpallPayment:
                 transaction = Payment.objects.create(
                     authority=self.authority, order=self.order
                 )
+                logger.info("Transaction created; Pending")
 
             __URL = f"https://sandbox.zarinpal.com/pg/StartPay/{self.authority}"
             return __URL
@@ -75,7 +87,7 @@ class OrderViewSet(ModelViewSet):
     filterset_fields = ["status"]
 
     def get_queryset(self):
-        return self.request.user.order_user.all()
+        return self.request.user.order_user.exclude(orderitem_order__product_store=None)
 
     @action(methods=["POST"], detail=False)
     def checkout(self, request):
@@ -89,10 +101,11 @@ class OrderViewSet(ModelViewSet):
         if payment.request():
             redirect_url = payment.payment()
             empty_cart(cart=request.user.cart_user)
+            logger.info("Connected to Zarinpal server")
             return Response(
                 status=status.HTTP_202_ACCEPTED, data={"payment_url": redirect_url}
             )
-
+        logger.error("Error during connect to Zarinpal server")
         return Response(status=status.HTTP_502_BAD_GATEWAY)
 
     @action(methods=["GET"], detail=False, permission_classes=[AllowAny])
@@ -106,11 +119,37 @@ class OrderViewSet(ModelViewSet):
         if state == "OK":
             order.status = order.OrderStatus.PAID
             transaction.status = transaction.PaymentStatus.PAID
+            minus_stock_item(order)
+            logger.info(f"Success payment for order id: {order.pk}")
+            logger.info("PAID ORDER")
         else:
             order.status = order.OrderStatus.CANCELED
             transaction.status = transaction.PaymentStatus.CANCELED
+            logger.info("CANCELED ORDER")
         order.save()
         transaction.save()
+        OrderItem.objects.change_status(order)
         redirect_url = "http://localhost:5173/profile/orders"
 
         return redirect(redirect_url)
+
+def minus_stock_item(order: Order):
+    from stock.models import ProductStore
+    order_items = order.orderitem_order.all()
+    for item in order_items:
+        item.product_store.stock -= item.quantity
+        item.product_store.save()
+
+
+class MyOrdersViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericViewSet):
+    queryset = OrderItem.objects.all()
+    permission_classes = [OrderItemSeller]
+    serializer_class = OrderItemSerializer
+    
+    def get_queryset(self):
+        return OrderItem.objects.filter(product_store__store__seller=self.request.user)
+    
+    @action(methods=["POST"], detail=True)
+    def change_status(self, request, pk=None):
+        logger.info(f"Order {pk} status changed by user: {request.user}")
+        return super().partial_update(request)
